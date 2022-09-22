a4a9faceb26c6d685d4d6c7760fbfae02baddb1a
lvergaram <lvergaramena@gmail.com>
2022-09-22 16:34:12 -0300

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Layout } from '../src/layouts'
+
+  
+export default function Home() {
+  return (
+    <>
+      <Layout>
+
+        <div>
+            hola mundo
+        </div>
+      </Layout>
+    </>
+  )
+}
---
+++ src/layouts/Layout.jsx
@@ -0,0 +1,25 @@
+import Head from "next/head"
+import { MainMenu } from "../components/UI/mainMenu"
+
+
+
+export const Layout = ({ children,title }) => {
+  return (
+    <>
+        <Head>
+            <title>{ title || "Poke Next"}</title>
+            <meta name="author" content="Luis Vergara"/>
+            <meta name="description" content="for educational purpose only"/>
+            <meta name="keywords" content="pokemon, game"/>
+
+        </Head>
+
+        <MainMenu />
+        <main>
+           
+            {children}
+        </main>
+
+    </>
+  )
+}
